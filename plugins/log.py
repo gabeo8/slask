19093bcbe4b6f9fee773bd691cdc00a0c541d738
Bill Mill <bill.mill@gmail.com>
2015-02-16 21:52:51 -0500

    add a simple logging plugin

---
+++ plugins/log.py
@@ -0,0 +1,16 @@
+#TODO: move to utils or something?
+def query(db, sql, *params):
+    c = db.cursor()
+    c.execute(sql, params)
+    rows = c.fetchall()
+    c.close()
+    db.commit()
+    return rows
+
+def on_message(msg, server):
+    db = server["db"]
+    query(db, "INSERT INTO log VALUES (?, ?, ?)", msg["text"], msg["user"], msg["ts"])
+
+def on_init(server):
+    db = server["db"]
+    query(db, "CREATE TABLE IF NOT EXISTS log (msg STRING, sender STRING, time STRING)")
