3fe1f3cb693250f8561b134674b8b2062e303ef9
Bill Mill <bill.mill@gmail.com>
2015-02-22 13:49:51 -0500

    add test for cmd line output

---
+++ test/test_cmd.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+import os
+import subprocess
+import shlex
+
+from nose.tools import eq_
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+TESTPLUGINS = os.path.join(DIR, "plugins")
+
+def sh(cmd):
+     proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+     output = proc.communicate()[0].decode("utf8")
+     ret = proc.returncode
+     return output, ret
+
+def test_cmd():
+    msg = u"!echo Iñtërnâtiônàlizætiøn"
+    out, res = sh(u"slask -c '{0}' --pluginpath {1}".format(msg, TESTPLUGINS).encode("utf8"))
+    out = out.strip()
+    eq_(out, msg)
+    eq_(res, 0)
